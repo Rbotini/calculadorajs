9606c14af8a0208f9edefb9806ac4435c26e357f
unknown <botinirenato@gmail.com>
2021-07-21 17:59:46 -0300

    criado a função que recebe os valores e reliza o calculo

--- js/calculadora.js
+++ js/calculadora.js
@@ -8,3 +8,14 @@ function clean(){
    document.getElementById('resultado').innerHTML=" ";
 
 }
+function calcular(){
+  var total = document.getElementById('resultado').innerHTML;
+  if(total)
+  {
+     
+    document.getElementById('resultado').innerHTML = eval(total);
+  }
+}
+  
+var x = '3 * 8 + 10';
+console.log(eval(x));
